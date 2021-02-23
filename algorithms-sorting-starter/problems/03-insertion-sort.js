c9bf86a89e23ded7c8efbe8a421463fe9aa6b9c1
Vishaal Meduri <vishaal.meduri@gmail.com>
2021-02-23 13:01:31 -0600

    Finished Insertion Sort

--- algorithms-sorting-starter/problems/03-insertion-sort.js
+++ algorithms-sorting-starter/problems/03-insertion-sort.js
@@ -2,7 +2,24 @@
 
 function insertionSort(list) {
   // your code here
+  let holePosition;
+  let valueToInsert;
+
+  for (let i = 1; i < list.length; i++) {
+    valueToInsert = list[i];
+    holePosition = i;
+
+    while(holePosition > 0 && list[holePosition - 1] > valueToInsert) {
+      list[holePosition] = list[holePosition - 1];
+      holePosition = holePosition - 1;
+    }
+
+    list[holePosition] = valueToInsert;
+  }
+  return list;
 }
+
+console.log(insertionSort([4,3,2,1]));
 
 module.exports = {
   insertionSort
