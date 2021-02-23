4705cf36f85bf3a19adf62e3e717efc37a49a12a
Vishaal Meduri <vishaal.meduri@gmail.com>
2021-02-23 16:09:37 -0600

    FINISHED

--- algorithms-sorting-starter/problems/04-merge-sort.js
+++ algorithms-sorting-starter/problems/04-merge-sort.js
@@ -2,55 +2,46 @@
 
 function merge(array1, array2) {
   // your code here
-  var result = [...array1, ...array2];
-  console.log(result);
-  while(array1.length > 0 && array2.length > 0){
+  let merged = [];
+  // console.log("array1", array1);
+  // console.log("array2", array2);
+  while(array1.length || array2.length) {
+    let ele1 = array1.length ? array1[0] : Infinity;
+    let ele2 = array2.length ? array2[0] : Infinity;
 
-    if(array1[0] > array2[0]){
-      result.push(array2[0]);
-      array2.splice(array2.indexOf(0), 1);
+    let next;
+    if(ele1 < ele2){
+      next = array1.shift();
     } else {
-      result.push(array1[0]);
-      array1.splice(array1.indexOf(0), 1);
+      next = array2.shift();
     }
 
+    merged.push(next);
+
   }
 
-  while(array1.length !== 0){
-    result.push(array1[0]);
-    array1.splice(array1.indexOf(0), 1);
-  }
-
-  while(array2.length !== 0){
-    result.push(array2[0]);
-    array2.splice(array2.indexOf(0), 1);
-  }
-
-  return result;
-
+  return merged;
 }
 
 //[4,3,2,1,9,3,0]
 
 function mergeSort(array) {
   // your code here
-  n = array.length;
+  if (array.length <= 1) {
+    return array;
+  }
 
-  if (n === 1) return array;
+  let midIdx = Math.floor(array.length / 2);
+  let leftHalf = array.slice(0, midIdx);
+  let rightHalf = array.slice(midIdx);
 
-  let midPoint = Math.floor(n/2);
+  console.log(leftHalf);
+  console.log(rightHalf);
 
-  var l1 = array.slice(0, midPoint);
+  let sortedLeft = mergeSort(leftHalf);
+  let sortedRight = mergeSort(rightHalf);
 
-  var l2 = array.slice(midPoint);  
-
-  //console.log(l1);
-  //console.log(l2);
-
-  l1 = mergeSort(l1);
-  l2 = mergeSort(l2);
-
-  return merge(l1, l2);
+  return merge(sortedLeft, sortedRight);
 }
 
 console.log(mergeSort([4,3,2,1,9,3,0]));
--- algorithms-sorting-starter/problems/05-quick-sort.js
+++ algorithms-sorting-starter/problems/05-quick-sort.js
@@ -2,8 +2,26 @@
 
 function quickSort(array) {
   // your code here
+  if (array.length === 0 || array.length === 1) return array;
+  let pivot = array[0];
+  array.shift();
+  let left = [];
+  let right = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < pivot) {
+      left.push(array[i]);
+    } else {
+      right.push(array[i]);
+    }
+  }
+
+  let leftSorted = quickSort(left);
+  let rightSorted = quickSort(right);
+
+  return [...leftSorted, pivot,...rightSorted];
 }
 
+console.log(quickSort([4,3,2,1,9,3,0]));
 
 module.exports = {
   quickSort
